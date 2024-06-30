59328e5f4efd13cd6084f8e3c5a27702a7232894
Devankkaranwal <devankkaranwal@gmail.com>
2024-06-30 13:47:00 +0530

    Initial Commit

---
+++ ParallaxView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParallaxView
+//
+//  Created by Devank on 30/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
